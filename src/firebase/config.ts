0561c0b1aebab73f3175320b12ff795a90b5787c
RITABRATO ROY <ritabrator@gmail.com>
2026-03-13 08:43:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "captionwise",
+  "appId": "1:528449173646:web:095d500f1bc38648feb1df",
+  "apiKey": "AIzaSyBJ-juS0Lnw2znIxwXkPH1RpXlr5hsgBEg",
+  "authDomain": "captionwise.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "528449173646"
+};
